4ee791f31b8636f6d3f7202d9694c16f2be77723
Emanuel Moraes <emanuelmoraes297@gmail.com>
2021-03-10 16:06:08 -0300

    fix include

--- src/i18n/export.sh
+++ src/i18n/export.sh
@@ -5,3 +5,20 @@ case "$_LANG" in
 	*)  source "$_SRC/i18n/lang/en/export.sh";;
 esac
 
+_show_error () {
+	local err="$1" &&
+	shift &&
+
+	case "$err" in
+		$_ERROR_ROOT_NOT_ALLOWED) printf "$_ERROR_MESSAGE_ROOT_NOT_ALLOWED\n" "$@" "$err";;
+		$_ERROR_INCLUDE) printf "$_ERROR_MESSAGE_INCLUDE\n" "$@" "$err";;
+		$_ERROR_EXTGLOB) printf "$_ERROR_MESSAGE_EXTGLOB\n" "$@" "$err";;
+		$_ERROR_INVALID_ARGUMENT) printf "$_ERROR_MESSAGE_INVALID_ARGUMENT\n" "$@" "$err";;
+		$_ERROR_INVALID_DISTRO) printf "$_ERROR_MESSAGE_INVALID_DISTRO\n" "$@" "$err";;
+		$_ERROR_INSTALL_DEPENDES) printf "$_ERROR_MESSAGE_INSTALL_DEPENDES\n" "$@" "$err";;
+		$_ERROR_INSTALL_MAKE_DEPENDES) printf "$_ERROR_MESSAGE_INSTALL_MAKE_DEPENDES\n" "$@" "$err";;
+		$_ERROR_UPDATES) printf "$_ERROR_MESSAGE_UPDATES\n" "$@" "$err";;
+		*) printf "$_ERROR_MESSAGE_UNEXPECTED\n" "$@" "$err";;
+	esac
+}
+
--- src/i18n/lang/pt/errors.sh
+++ src/i18n/lang/pt/errors.sh
@@ -34,20 +34,3 @@ _ERROR_MESSAGE_UPDATES="${_THEME_ERROR}\
 ${0##*/}: ERROR: UPDATES: Não foi possível atualizar os pacotes do sistema! [Código %s]\
 ${_THEME_ERROR_END}"
 
-_show_error () {
-	local err="$1" &&
-	shift &&
-
-	case "$err" in
-		$_ERROR_ROOT_NOT_ALLOWED) printf "$_ERROR_MESSAGE_ROOT_NOT_ALLOWED\n" "$@" "$err";;
-		$_ERROR_INCLUDE) printf "$_ERROR_MESSAGE_INCLUDE\n" "$@" "$err";;
-		$_ERROR_EXTGLOB) printf "$_ERROR_MESSAGE_EXTGLOB\n" "$@" "$err";;
-		$_ERROR_INVALID_ARGUMENT) printf "$_ERROR_MESSAGE_INVALID_ARGUMENT\n" "$@" "$err";;
-		$_ERROR_INVALID_DISTRO) printf "$_ERROR_MESSAGE_INVALID_DISTRO\n" "$@" "$err";;
-		$_ERROR_INSTALL_DEPENDES) printf "$_ERROR_MESSAGE_INSTALL_DEPENDES\n" "$@" "$err";;
-		$_ERROR_INSTALL_MAKE_DEPENDES) printf "$_ERROR_MESSAGE_INSTALL_MAKE_DEPENDES\n" "$@" "$err";;
-		$_ERROR_UPDATES) printf "$_ERROR_MESSAGE_UPDATES\n" "$@" "$err";;
-		*) printf "$_ERROR_MESSAGE_UNEXPECTED\n" "$@" "$err";;
-	esac
-}
-
